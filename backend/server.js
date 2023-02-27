eee10c34c486a0a9028300e598ea4d74a6ac803c
talist225 <talist225@gmail.com>
2023-02-28 00:54:34 +0200

    s

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "/build")));
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -33,8 +35,6 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-app.use(express.static(path.join(__dirname, "/build")));
 
 // app.get("*", (req, res) =>
 //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
